b16c45eba1835de6e00acd2a04f2266fd377e5d3
hyakusho <hyakusho.io@gmail.com>
2020-01-21 15:36:59 +0900

    Add allowed networks

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allowed networks
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end

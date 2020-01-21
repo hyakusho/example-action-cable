646033a9fdd36142bfae6acd3638d4d46df7588a
hyakusho <hyakusho.io@gmail.com>
2020-01-21 15:10:18 +0900

    Rails new

---
+++ docker/rails/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+set -xeuo pipefail
+
+[ ! -f "Gemfile" ] && rails new --skip .
+
+bundle install --jobs=4 --retry=3
+
+[ "$RAILS_ENV" = "development" ] && yarn install --check-files
+
+exec "$@"
